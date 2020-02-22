586fda14bc0b52b62e1669cf1018aff941b4a044
Matthew Jones <matts3371@gmail.com>
2020-02-22 19:54:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/' do 
+    "This is my home"
+  end
+  
+  get '/reversename/:name' do 
+    @name = params[:name].to_s.reverse 
+    "#{@name}"
+  end 
+  
+  get '/square/:number' do 
+    @number = params[:number].to_i 
+    @square = @number ** 2 
+    "#{@square}"
+  end 
+  
+  get 
+    
 
 end
