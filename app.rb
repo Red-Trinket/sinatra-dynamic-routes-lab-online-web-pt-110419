b79dac0fcdb8731dc91ff47eeccd628638e67a71
Matthew Jones <matts3371@gmail.com>
2020-02-22 20:07:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,11 +21,13 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @number = params[:number].to_i 
     @phrase = params[:phrase].to_s 
+    @str = "" 
     n = 0 
     while n < @number do 
-      puts "#{@phrase}"
+      @str << "#{@phrase}\n"
       n = n + 1 
     end 
+    "#{@str}"
   end 
 
 end
